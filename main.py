176f2b9af283c16ff762db1eaea6933f3f983878
Kenneth <kenny@kennethchilds.com>
2024-03-28 20:31:34 -0400

    claude haiku w/ streaming

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-# Load environment variables
+import anthropic
 import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-
-# CLAUDE 3
-import anthropic
+    
+user_input = input("What do you want to know about Unix?: ")
 
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=ANTHROPIC_API_KEY,
 )
 
-message = client.messages.create(
-    model="claude-3-opus-20240229",
+with client.messages.stream(
+    model="claude-3-haiku-20240307",
     max_tokens=1000,
     temperature=0.0,
-    system="Respond only in Yoda-speak.",
+    system="""
+        *nixbot is an AI-powered assistant designed to guide users 
+        in customizing their operating system environments for enhanced 
+        productivity and aesthetic appeal.
+        
+        Try to give the easiest and most direct way to achieve the user's goal, 
+        such as telling users how to install arch linux via archinstall.
+        
+        Keep information VERY concise and to the point.
+    """,
     messages=[
-        {"role": "user", "content": "How are you today?"}
+        {"role": "user", "content": f"{user_input}"}
     ]
-)
+) as stream:
+    for text in stream.text_stream:
+        print(text, end="", flush=True)
 
-print(message.content)
